64931795aa1e9402d7f67d2454c9d616f3beb5e3
singuerinc <nahuel.scotti@gmail.com>
2017-11-21 23:44:41 +0100

    fix: Typo

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -19,7 +19,7 @@ export default ({ data, pathContext }) => {
       <header className="mb5">
         <span className="fw3 gray f5">{date}</span>
         <span className="fw3 dark-gray f6"> by {post.frontmatter.author}</span>
-        <h1 className="f1 fw3 mv1 db red">
+        <h1 className="f1 fw2 mv1 db green">
           {post.frontmatter.title}{' '}
         </h1>
         <span className="f6 gray">
@@ -33,7 +33,7 @@ export default ({ data, pathContext }) => {
         />
       </section>
       <sections>
-        <h4 className="ma0 mt4 pa0 tc">Share this brashtard</h4>
+        <h4 className="ma0 mt4 pa0 tc">Share this bashtard</h4>
         <div className="flex justify-center pv3">
           <a className="dib link dim dark-gray mr3 tr" href={`https://www.facebook.com/sharer/sharer.php?u=${link}+${hashtag}`} target="_blank"><span dangerouslySetInnerHTML={{ __html: feather.icons.facebook.toSvg() }} /></a>
           <a className="dib link dim dark-gray ml3 tl" href={`https://twitter.com/home?status=${link}+${hashtag}`} target="_blank"><span dangerouslySetInnerHTML={{ __html: feather.icons.twitter.toSvg() }} /></a>
