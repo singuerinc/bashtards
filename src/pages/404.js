c5179d2569044c8fa5a83cafe508d196f678e6dc
Ricardo Saiz <ricardo.saiz@gmail.com>
2017-11-22 12:39:47 +0100

    add i-van gif on 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="f2 fw3 white-50 ma0">404 - NOT FOUND</h1>
+    <p className="f4 fw3 white-50 mt3">You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <img className="mv4" src="https://media.giphy.com/media/HyN3hNRqSPkic/200.gif" alt="gif error" />
   </div>
 )
 
