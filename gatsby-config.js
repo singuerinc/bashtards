16eb72dc4eb60703310f46316a968a2102dc33a0
singuerinc <nahuel.scotti@gmail.com>
2017-11-22 22:00:19 +0100

    feat: Add RSS, welcome to 1990!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: `> Bashtards`,
+    title: `Bashtards`,
+    description: `A blog about themes, wallpapers, fonts and editors for coders.`,
+    siteUrl: `https://camaleon.singuerinc.com`
   },
   plugins: [
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-881783-19',
-      },
+        trackingId: 'UA-881783-19'
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
-        path: `${__dirname}/src/`,
-      },
+        path: `${__dirname}/src/`
+      }
     },
     `gatsby-plugin-react-helmet`,
     {
@@ -24,11 +26,65 @@ module.exports = {
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
-              classPrefix: 'language-',
+              classPrefix: 'language-'
+            }
+          }
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                });
+              });
             },
-          },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [fields___date] },
+                  limit: 1000
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields {
+                        slug
+                        date
+                      }
+                      frontmatter {
+                        title
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml'
+          }
         ]
       }
     }
-  ],
-}
+  ]
+};
